cf277a9d39cab8563d237b88e78273624896cbaf
Yan Zhang <2351748+Eskibear@users.noreply.github.com>
2023-09-15 09:19:55 +0800

    Set same default retry options with other providers (#7)

--- src/AzureAppConfigurationOptions.ts
+++ src/AzureAppConfigurationOptions.ts
@@ -4,6 +4,9 @@
 import { AppConfigurationClientOptions } from "@azure/app-configuration";
 import { AzureAppConfigurationKeyVaultOptions } from "./keyvault/AzureAppConfigurationKeyVaultOptions";
 
+export const MaxRetries = 2;
+export const MaxRetryDelayInMs = 60000;
+
 export interface AzureAppConfigurationOptions {
     selectors?: { keyFilter: string, labelFilter: string }[];
     trimKeyPrefixes?: string[];
