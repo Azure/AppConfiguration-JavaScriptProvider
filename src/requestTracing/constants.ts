a29a77bb83a6fb39c3231f244b197cef01527301
Yan Zhang <yanzh@microsoft.com>
2023-09-20 16:35:09 +0800

    address comments:move version constant out

--- src/requestTracing/constants.ts
+++ src/requestTracing/constants.ts
@@ -1,10 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+import { Version } from "../version";
+
 export const RequestTracingDisabledEnvironmentVariable = "AZURE_APP_CONFIGURATION_TRACING_DISABLED";
 
 // User Agent
-export const Version = "0.1.0-preview";
 export const UserAgentPrefix = `javascript-appconfiguration-provider/${Version}`;
 
 // Correlation Context
