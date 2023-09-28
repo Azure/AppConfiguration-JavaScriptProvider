0dee86900e9bd7ae4142a0ba08394682b2eff378
Yan Zhang <yanzh@microsoft.com>
2023-09-28 15:49:46 +0800

    update secretReference.mjs to reuse same credential

--- examples/secretReference.mjs
+++ examples/secretReference.mjs
@@ -5,12 +5,11 @@ import * as dotenv from "dotenv";
 dotenv.config()
 
 /**
- * This example retrives all settings and resolve secret value from keyvault.
- * Before you run it, please add a sample keyvault secret reference "app.secret".
+ * Before you run it, please add a Key Vault reference with key "app.secret" in your App Configuration store.
+ * This example uses the same identity to authenticate with both App Configuration and Key Vault. Make sure that this identity has access to read secrets from Key Vault.
  * Value of secret "app.secret" will be printed.
  *
  * Below environment variables are required for this example:
- * - APPCONFIG_CONNECTION_STRING
  * - APPCONFIG_ENDPOINT
  * - AZURE_TENANT_ID
  * - AZURE_CLIENT_ID
@@ -19,10 +18,11 @@ dotenv.config()
 
 import { load } from "@azure/app-configuration-provider";
 import { getDefaultAzureCredential } from "@azure/identity";
-const connectionString = process.env.APPCONFIG_CONNECTION_STRING;
-const settings = await load(connectionString, {
+const endpoint = process.env.APPCONFIG_ENDPOINT;
+const credential = getDefaultAzureCredential();
+const settings = await load(endpoint, credential, {
     keyVaultOptions: {
-        credential: getDefaultAzureCredential()
+        credential: credential
     }
 });
 const secretKey = "app.secret";
