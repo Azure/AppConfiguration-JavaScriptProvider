c176593f13721608f7d20185dbf00a151d64f915
Yan Zhang <yanzh@microsoft.com>
2024-01-30 14:22:35 +0800

    turn off debug mode, explicitly echo command instead

--- scripts/build-and-pack.sh
+++ scripts/build-and-pack.sh
@@ -3,9 +3,6 @@
 # Stop on error.
 set -e
 
-# Echo commands.
-set -x
-
 # Get the directory of the script.
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
@@ -16,9 +13,15 @@ PROJECT_BASE_DIR=$(dirname $SCRIPT_DIR)
 cd $PROJECT_BASE_DIR
 
 # Install dependencies, build, and test.
+echo "npm clean install"
 npm ci
+
+echo "npm run build"
 npm run build
+
+echo "npm run test"
 npm run test
 
 # Create a tarball.
+echo "npm pack"
 npm pack
