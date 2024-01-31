db666a21a36b1f7dab7c33ad33712a983fb21d02
Yan Zhang <yanzh@microsoft.com>
2024-01-31 11:09:55 +0800

    rename members to adopt private properties

--- src/common/disposable.ts
+++ src/common/disposable.ts
@@ -2,14 +2,17 @@
 // Licensed under the MIT license.
 
 export class Disposable {
-    private disposed = false;
-    constructor(private callOnDispose: () => any) { }
+    #disposed = false;
+    #callOnDispose: () => any;
 
-    dispose() {
-        if (!this.disposed) {
-            this.callOnDispose();
-        }
-        this.disposed = true;
+    constructor(callOnDispose: () => any) {
+        this.#callOnDispose = callOnDispose;
     }
 
+    dispose() {
+        if (!this.#disposed) {
+            this.#callOnDispose();
+        }
+        this.#disposed = true;
+    }
 }
