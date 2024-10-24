27b6e5659a8acdcf9aaa516019e88c2160d595d3
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2024-10-24 13:32:48 +0800

    add requestTracingOptions

--- src/AzureAppConfigurationOptions.ts
+++ src/AzureAppConfigurationOptions.ts
@@ -6,6 +6,7 @@ import { KeyVaultOptions } from "./keyvault/KeyVaultOptions.js";
 import { RefreshOptions } from "./RefreshOptions.js";
 import { SettingSelector } from "./types.js";
 import { FeatureFlagOptions } from "./featureManagement/FeatureFlagOptions.js";
+import { RequestTracingOptions } from "./requestTracing/RequestTracingOptions.js";
 
 export const MaxRetries = 2;
 export const MaxRetryDelayInMs = 60000;
@@ -47,4 +48,9 @@ export interface AzureAppConfigurationOptions {
      * Specifies options used to configure feature flags.
      */
     featureFlagOptions?: FeatureFlagOptions;
+
+    /**
+     * Specifies options used to configure request tracing.
+     */
+    requestTracingOptions?: RequestTracingOptions;
 }
---
+++ src/requestTracing/RequestTracingOptions.ts
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ * Options used to configure request tracing.
+ */
+export interface RequestTracingOptions {
+    /**
+     * Specifies whether request tracing is enabled.
+     */
+    enabled: boolean;
+}
