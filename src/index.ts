75036884aa0cd5878d97a08e4f6a026f5eb82458
Zhiyuan Liang <141655842+zhiyuanliang-ms@users.noreply.github.com>
2024-10-31 14:23:51 +0800

    export loadFromCdn (#118)

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@
 
 export { AzureAppConfiguration } from "./AzureAppConfiguration.js";
 export { Disposable } from "./common/disposable.js";
-export { load } from "./load.js";
+export { load, loadFromCdn } from "./load.js";
 export { KeyFilter, LabelFilter } from "./types.js";
 export { VERSION } from "./version.js";
