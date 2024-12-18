48585042a65f9174f4d435834928086842d77205
Zhiyuan Liang <141655842+zhiyuanliang-ms@users.noreply.github.com>
2024-12-18 11:28:11 +0800

    Add feature filter usage tracing (#108)
    * wip
    
    * WIP
    
    * add integration test
    
    * fix lint
    
    * update port
    
    * update port
    
    * update
    
    * update
    
    * avoid create new featureFlagTracing object when request tracing is disabled
    
    * update
    
    * update
    
    * add variant and telemetry tracing

--- src/featureManagement/constants.ts
+++ src/featureManagement/constants.ts
@@ -3,3 +3,14 @@
 
 export const FEATURE_MANAGEMENT_KEY_NAME = "feature_management";
 export const FEATURE_FLAGS_KEY_NAME = "feature_flags";
+export const CONDITIONS_KEY_NAME = "conditions";
+export const CLIENT_FILTERS_KEY_NAME = "client_filters";
+export const TELEMETRY_KEY_NAME = "telemetry";
+export const VARIANTS_KEY_NAME = "variants";
+export const ALLOCATION_KEY_NAME = "allocation";
+export const SEED_KEY_NAME = "seed";
+export const NAME_KEY_NAME = "name";
+export const ENABLED_KEY_NAME = "enabled";
+
+export const TIME_WINDOW_FILTER_NAMES = ["TimeWindow", "Microsoft.TimeWindow", "TimeWindowFilter", "Microsoft.TimeWindowFilter"];
+export const TARGETING_FILTER_NAMES = ["Targeting", "Microsoft.Targeting", "TargetingFilter", "Microsoft.TargetingFilter"];
