a26417b5189cfeed1e80cc7a38f2ed37a09be5c1
zhiyuanliang <zhiyuanliang@microsoft.com>
2025-01-16 13:59:28 +0800

    add express example

---
+++ examples/server.mjs
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import * as dotenv from "dotenv";
+dotenv.config()
+
+import { load } from "@azure/app-configuration-provider";
+const connectionString = process.env.APPCONFIG_CONNECTION_STRING;
+const appConfig = await load(connectionString, {
+    refreshOptions: {
+        enabled: true
+    }
+});
+
+appConfig.onRefresh(() => {
+    console.log("Config refreshed.");
+});
+
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    appConfig.refresh();
+    res.send("Please go to /config to get the configuration.");
+});
+
+app.get("/config", (req, res) => {
+    appConfig.refresh();
+    res.json(appConfig.constructConfigurationObject());
+});
+
+app.get("/config/:key", (req, res) => {
+    appConfig.refresh();
+    res.json(appConfig.get(req.params.key) ?? "");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
