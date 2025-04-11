7b73f00bf609f5300b7c4eced301f56e526672e4
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2025-04-11 10:48:07 +0800

    fix indent

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -2,9 +2,9 @@
 // Licensed under the MIT license.
 
 export function shuffleList<T>(array: T[]): T[] {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
