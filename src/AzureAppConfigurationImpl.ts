e36e0670f5f7d5ea4e73301c9f39b96165d086e1
Zhiyuan Liang <141655842+zhiyuanliang-ms@users.noreply.github.com>
2025-08-07 11:17:27 +0800

    Support tag filter (#188)
    * support snapshot
    
    * add testcase
    
    * add testcase
    
    * fix lint
    
    * update
    
    * wip
    
    * wip
    
    * support tag filter
    
    * add test
    
    * update test
    
    * update testcase
    
    * update
    
    * add more testcases
    
    * update
    
    * fix lint
    
    * update testcase
    
    * update
    
    * add more testcases
    
    * correct null tag test

--- src/AzureAppConfigurationImpl.ts
+++ src/AzureAppConfigurationImpl.ts
@@ -491,7 +491,8 @@ export class AzureAppConfigurationImpl implements AzureAppConfiguration {
                 if (selector.snapshotName === undefined) {
                     const listOptions: ListConfigurationSettingsOptions = {
                         keyFilter: selector.keyFilter,
-                        labelFilter: selector.labelFilter
+                        labelFilter: selector.labelFilter,
+                        tagsFilter: selector.tagFilters
                     };
                     const pageEtags: string[] = [];
                     const pageIterator = listConfigurationSettingsWithTrace(
@@ -727,6 +728,7 @@ export class AzureAppConfigurationImpl implements AzureAppConfiguration {
                 const listOptions: ListConfigurationSettingsOptions = {
                     keyFilter: selector.keyFilter,
                     labelFilter: selector.labelFilter,
+                    tagsFilter: selector.tagFilters,
                     pageEtags: selector.pageEtags
                 };
 
@@ -966,7 +968,11 @@ function getValidSettingSelectors(selectors: SettingSelector[]): SettingSelector
     // below code deduplicates selectors, the latter selector wins
     const uniqueSelectors: SettingSelector[] = [];
     for (const selector of selectors) {
-        const existingSelectorIndex = uniqueSelectors.findIndex(s => s.keyFilter === selector.keyFilter && s.labelFilter === selector.labelFilter && s.snapshotName === selector.snapshotName);
+        const existingSelectorIndex = uniqueSelectors.findIndex(
+            s => s.keyFilter === selector.keyFilter &&
+                s.labelFilter === selector.labelFilter &&
+                s.snapshotName === selector.snapshotName &&
+                areTagFiltersEqual(s.tagFilters, selector.tagFilters));
         if (existingSelectorIndex >= 0) {
             uniqueSelectors.splice(existingSelectorIndex, 1);
         }
@@ -976,8 +982,8 @@ function getValidSettingSelectors(selectors: SettingSelector[]): SettingSelector
     return uniqueSelectors.map(selectorCandidate => {
         const selector = { ...selectorCandidate };
         if (selector.snapshotName) {
-            if (selector.keyFilter || selector.labelFilter) {
-                throw new ArgumentError("Key or label filter should not be used for a snapshot.");
+            if (selector.keyFilter || selector.labelFilter || selector.tagFilters) {
+                throw new ArgumentError("Key, label or tag filters should not be specified while selecting a snapshot.");
             }
         } else {
             if (!selector.keyFilter) {
@@ -989,9 +995,29 @@ function getValidSettingSelectors(selectors: SettingSelector[]): SettingSelector
             if (selector.labelFilter.includes("*") || selector.labelFilter.includes(",")) {
                 throw new ArgumentError("The characters '*' and ',' are not supported in label filters.");
             }
+            if (selector.tagFilters) {
+                validateTagFilters(selector.tagFilters);
+            }
         }
         return selector;
     });
+}
+
+function areTagFiltersEqual(tagsA?: string[], tagsB?: string[]): boolean {
+    if (!tagsA && !tagsB) {
+        return true;
+    }
+    if (!tagsA || !tagsB) {
+        return false;
+    }
+    if (tagsA.length !== tagsB.length) {
+        return false;
+    }
+
+    const sortedStringA = [...tagsA].sort().join("\n");
+    const sortedStringB = [...tagsB].sort().join("\n");
+
+    return sortedStringA === sortedStringB;
 }
 
 function getValidKeyValueSelectors(selectors?: SettingSelector[]): SettingSelector[] {
@@ -1013,4 +1039,13 @@ function getValidFeatureFlagSelectors(selectors?: SettingSelector[]): SettingSel
         }
     });
     return getValidSettingSelectors(selectors);
+}
+
+function validateTagFilters(tagFilters: string[]): void {
+    for (const tagFilter of tagFilters) {
+        const res = tagFilter.split("=");
+        if (res[0] === "" || res.length !== 2) {
+            throw new Error(`Invalid tag filter: ${tagFilter}. Tag filter must follow the format "tagName=tagValue".`);
+        }
+    }
 }
